37f29750dd18bcdd0e179caa63214c58d7a910ce
Mostafa Nassr <mostafa.nassr88@gmail.com>
2021-08-04 19:36:42 -0400

    added the Ranks component to show the summoner's solo / flex ranks to the UI

---
+++ src/components/Ranks.js
@@ -0,0 +1,49 @@
+import { Tooltip } from '@material-ui/core';
+import './Ranks.css';
+
+const Ranks = ({ranks}) => {
+    const soloRank = ranks.find(r => r.queueType === 'Ranked Solo');
+    const flexRank = ranks.find(r => r.queueType === 'Flex 5:5 Rank');
+    return (
+        <div className="Ranks">
+            <div className="Ranks-solo">
+                <Tooltip title="Ranked Solo" arrow className="Ranks-img">
+                    <img src={soloRank ? `/img/ranks/${soloRank.tier}.png` : '/img/ranks/UNRANKED.png'} alt="rank" />
+                </Tooltip>
+                <div>
+                    <p><b>Ranked Solo</b></p>
+                    {soloRank ? (
+                    <>
+                        <p style={{color: "#0e95eb"}}>
+                            <b>{soloRank.tier[0] + soloRank.tier.slice(1).toLowerCase()} {soloRank.tier === 'MASTER' || soloRank.tier === 'GRANDMASTER' || soloRank.tier === 'CHALLENGER' ? null : soloRank.rank}</b>
+                        </p>
+                        <p>{soloRank.leaguePoints} LP</p>
+                        <p>{soloRank.wins}W / {soloRank.losses}L</p>
+                        <p>{Math.round(soloRank.wins / (soloRank.wins + soloRank.losses) * 100)}% Win Ratio</p>
+                    </> 
+                    ) : <p>Unranked</p>}
+                </div>
+            </div>
+            <div className="Ranks-flex">
+                <Tooltip title="Ranked Flex" arrow className="Ranks-img">
+                    <img src={flexRank ? `/img/ranks/${flexRank.tier}.png` : '/img/ranks/UNRANKED.png'} alt="rank" />
+                </Tooltip>
+                <div>
+                    <p><b>Flex 5:5 Rank</b></p>
+                    {flexRank ? (
+                    <>
+                        <p style={{color: "#0e95eb"}}>
+                            <b>{flexRank.tier[0] + flexRank.tier.slice(1).toLowerCase()} {flexRank.tier === 'MASTER' || flexRank.tier === 'GRANDMASTER' || flexRank.tier === 'CHALLENGER' ? null : flexRank.rank}</b>
+                        </p>
+                        <p>{flexRank.leaguePoints} LP</p>
+                        <p>{flexRank.wins}W / {flexRank.losses}L</p>
+                        <p>{Math.round(flexRank.wins / (flexRank.wins + flexRank.losses) * 100)}% Win Ratio</p>
+                    </> 
+                    ) : <p>Unranked</p>}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Ranks;
--- src/components/SummonerProfile.js
+++ src/components/SummonerProfile.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import getSummonerFromAPI from '../api';
 import Spinner from './Spinner';
 import Navbar from './Navbar';
 import ErrorPage from './ErrorPage';
 import Profile from './Profile';
-import './SummonerProfile.css';
+import Ranks from './Ranks';
 
 // regions supported by the League of Legends API
 const SUPPORTED_REGIONS = ["na1", "br1", "la1", "la2", "oc1", "kr", "jp1", "eun1", "euw1", "tr1", "ru"];
@@ -47,11 +47,18 @@ const SummonerProfile = () => {
                         {error
                         ? <ErrorPage msg={error} />
                         : (
-                            <Profile 
-                                name={summoner.name} 
-                                summonerProfile={summoner.profile}
-                                lastUpdated={summoner.lastUpdated} 
-                            />
+                            <Grid>
+                                <Grid container item xs={12}>
+                                    <Profile 
+                                        name={summoner.name} 
+                                        summonerProfile={summoner.profile}
+                                        lastUpdated={summoner.lastUpdated} 
+                                    />
+                                </Grid>
+                                <Grid container item xs={3}>
+                                    <Ranks ranks={summoner.rank} />
+                                </Grid>
+                            </Grid>
                         )}
                     </>
                 )}
